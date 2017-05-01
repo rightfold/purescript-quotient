79f60b3904bf9c87f4b89c3a015fc60f6e69fdb2
rightfold <rightfold@gmail.com>
2017-05-01 16:30:21 +0200

    Add Id, Abs, and Mod

--- src/Type/Quotient.purs
+++ src/Type/Quotient.purs
@@ -7,9 +7,15 @@ module Type.Quotient
   , type (/)
   , mkQuotient
   , runQuotient
+
+  , Id
+  , Abs
+  , Mod
   ) where
 
+import Data.Ord (abs)
 import Prelude
+import Type.Nat1 (kind Nat1, class IsNat1, N1Proxy(..), reifyNat1)
 import Type.Proxy (Proxy(..))
 
 --------------------------------------------------------------------------------
@@ -43,3 +49,23 @@ instance ordQuotient :: (Ord a, Canonical a e) => Ord (a / e) where
 
 instance showQuotient :: (Show a, Canonical a e) => Show (a / e) where
   show a = "(mkQuotient " <> show a <> ")"
+
+--------------------------------------------------------------------------------
+
+-- | `T / Id ~ T`.
+foreign import data Id :: Type
+
+-- | Negative values are equivalent to their positive counterparts.
+foreign import data Abs :: Type
+
+-- | Integers modulo some positive number.
+foreign import data Mod :: Nat1 -> Type
+
+instance canonicalId :: Canonical a Id where
+  canonical _ = id
+
+instance canonicalAbs :: (Ord a, Ring a) => Canonical a Abs where
+  canonical _ = abs
+
+instance canonicalMod :: (IsNat1 n) => Canonical Int (Mod n) where
+  canonical _ a = a `mod` reifyNat1 (N1Proxy :: N1Proxy n)
